a8fe96280c3060b60387bc3a2110bebba333c03d
LanaLob <lana_lob@mail.ru>
2020-06-02 04:33:43 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,35 +1,22 @@
-from distutils.core import setup
-setup(
-  name = 'randan',
-  packages = ['randan'],
-  version = '0.1',
-  license='MIT',
-  description = 'A python package for the analysis of social data',
-  author = 'Svetlana Zhuchkova',
-  author_email = 'lana_lob@mail.ru',
-  url = 'https://github.com/LanaLob/randan',
-  download_url = 'https://github.com/LanaLob/randan/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['Sociology', 'SPSS', 'Statistical methods', 'CHAID'],
-  install_requires=[            # I get to this in a second
-          'numpy',
-          'pandas',
-          'scipy',
-          'matplotlib',
-          'scikit-learn',
-          'statsmodels',
-          'factor_analyzer'
-          'pydot',
-          'graphviz'
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',
-    'Intended Audience :: Developers',
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
-  ],
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="randan-Lana_Lob", # Replace with your own username
+    version="0.0.1",
+    author="Svetlana Zhuchkova",
+    author_email="lana_lob@mail.ru",
+    description="A python package for the analysis of social data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LanaLob/randan",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
