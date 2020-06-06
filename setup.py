3852c5e1319dbae4324c6d0d36a6b0abedda89bc
LanaLob <lana_lob@mail.ru>
2020-06-06 18:42:39 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="randan",
-    version="0.0.1",
+    name="randan", # Replace with your own username
+    version="0.0.3",
     author="Svetlana Zhuchkova",
     author_email="lana_lob@mail.ru",
     description="A python package for the analysis of social data",
@@ -17,6 +17,16 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'numpy>=1.18.4',
+        'pandas>=1.0.3',
+        'scikit-learn>=0.23.1',
+        'statsmodels>=0.11.1',
+        'matplotlib>=3.1.1',
+        'factor_analyzer',
+        'pydot>=1.4.1',
+        'graphviz>=0.14'
     ],
     python_requires='>=3.6',
 )
