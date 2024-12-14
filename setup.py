7b28b1355da58e9d29fd40ce726b0ee71d7cce07
RandanCSS <80287949+RandanCSS@users.noreply.github.com>
2024-12-14 21:25:15 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,11 +10,9 @@ setuptools.setup(
     author="Aleksei Rotmistrov, Svetlana Zhuchkova",
     author_email="alexey.n.rotmistrov@gmail.com, lana_lob@mail.ru",
     description="A python package for gaining social and financial data and their analysis",
-    long_description='randan is a python package that aims to help social scientists, statisticians and financier. For the former ones it provides twelve analytical modules that emulate the most popular options presented in SPSS. Unlike the other python packages for data analysis, it has three main features, which make it attractive for social scientists:
-        1. it provides the results of the analysis in a readable and understandable form, similar to SPSS
-        2. it provides information about statistical significance of the parameters whenever possible
-        3. it meets the most popular analytical needs of social scientists; so, the switching among different packages and software stays in the past
-        A new -- thirteenth -- module provides data from YouTube literally by couple of clicks.',
+    long_description='randan is a python package that aims to help social scientists, statisticians and financiers.'
+        , 'For the former ones it provides twelve analytical modules that emulate the most popular options presented in SPSS.'
+        , 'A new -- thirteenth -- module provides data from YouTube literally by couple of clicks.',
     long_description_content_type="text/markdown",
     url="https://github.com/RandanCSS/randan",
     packages=setuptools.find_packages(),
