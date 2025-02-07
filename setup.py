32988c912b13242fcec95cc80ccdc09b8095719a
RandanCSS <80287949+RandanCSS@users.noreply.github.com>
2025-02-08 01:29:58 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
     install_requires=[
         'factor_analyzer',
         'google-api-python-client>=2.154.0',
+        'googledrivedownloader>=0.4',
         'graphviz>=0.14',
         'matplotlib>=3.1.1',
         'numpy>=1.18.4',
