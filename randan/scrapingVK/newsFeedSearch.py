8dd3f28ec61880d9e3a1784b9a0f94b8d4a5424e
RandanCSS <80287949+RandanCSS@users.noreply.github.com>
2025-02-15 22:50:20 +0300

    Update newsFeedSearch.py

--- randan/scrapingVK/newsFeedSearch.py
+++ randan/scrapingVK/newsFeedSearch.py
@@ -744,6 +744,6 @@ def newsFeedSearch(
     sys.exit()
 
 # warnings.filterwarnings("ignore")
-# print('Сейчас появится надпись: "An exception has occurred, use %tb to see the full traceback.\nSystemExit" -- так и должно быть)
+# print('Сейчас появится надпись: "An exception has occurred, use %tb to see the full traceback.\nSystemExit" -- так и должно быть')
 # input()
 # sys.exit()
