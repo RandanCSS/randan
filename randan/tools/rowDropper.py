3d70edefe929ebf9803afbb82d2181c8b4c2022d
RandanCSS <80287949+RandanCSS@users.noreply.github.com>
2025-03-15 18:42:46 +0300

    Update rowDropper.py

--- randan/tools/rowDropper.py
+++ randan/tools/rowDropper.py
@@ -1,13 +1,21 @@
+from randan.tools import varPreprocessor
 df = df.drop([307, 1037, 1339, 1420, 1433])
-columnsToJSON = list(df.columns) # все столбцы датафрейма `df` записать в объект columnsToJSON , причём отнести класс этого объекта к списку методом класса списков list()
+# Проверка всех столбцов на наличие в их ячейках JSON-формата
+from randan.tools import varPreprocessor
+columnsToJSON = varPreprocessor.jsonChecker(df)
 
-for column in df.columns:
-    if df[column].apply(lambda содержимоеПроверяемойЯчейки: True if (type(содержимоеПроверяемойЯчейки) == dict) | (type(содержимоеПроверяемойЯчейки) == list) else False).sum() == 0:
-        columnsToJSON.remove(column)
-
-if len(columnsToJSON) > 0: # проверка, что список columnsToJSON содержит более, чем 0 элементов
+if len(columnsToJSON) > 0:
     print('Обнаружены столбцы, содержащие внутри своих ячеек JSON-объекты; Excel не поддерживает JSON-формат;'
           , 'чтобы формат JSON не потерялся, сохраняю эти столбцы в файл формата НЕ XLSX, а JSON. Остальные же столбцы сохраняю в файл формата XLSX')
 
-    df.drop(columnsToJSON, axis=1).to_excel(r"YT_раздельный сбор\20250311_0230_YT_раздельный сбор_videos_Other_varS.xlsx")
+    columnsToJSON.append('id')
+    if 'from_id' in df.columns: columnsToJSON.append('from_id')
+    if 'owner_id' in df.columns: columnsToJSON.append('owner_id')
+
     df[columnsToJSON].to_json(r"YT_раздельный сбор\20250311_0230_YT_раздельный сбор_videos_JSON_varS.json")
+
+    columnsToJSON.remove('id')
+    if 'from_id' in columnsToJSON: columnsToJSON.remove('from_id')
+    if 'owner_id' in columnsToJSON: columnsToJSON.remove('owner_id')
+
+    df.drop(columnsToJSON, axis=1).to_excel(r"YT_раздельный сбор\20250311_0230_YT_раздельный сбор_videos_Other_varS.xlsx")
