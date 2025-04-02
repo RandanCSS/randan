55216520e54a21180a23f7fa0ba0b1532a94200f
RandanCSS <80287949+RandanCSS@users.noreply.github.com>
2025-04-02 21:29:32 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import os
 
 name="randan"
-version="1.3.29"
+version="1.3.30"
 slash = '\\' if os.name == 'nt' else '/' # выбор слэша в зависимости от ОС
 
 folderS = []
