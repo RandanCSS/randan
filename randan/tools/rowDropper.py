6ad7b7fe87233e6ec3635ba9571d11e052f7cdfe
Artem Minkin <aminkin@rutoken.ru>
2025-08-13 18:23:36 +0300

    NEW Init commit

--- randan/tools/rowDropper.py
+++ randan/tools/rowDropper.py
@@ -1,21 +1,32 @@
 from randan.tools import varPreprocessor
+
 df = df.drop([904, 2173, 2392, 2822, 3252, 3558, 3865, 3925])
 # Проверка всех столбцов на наличие в их ячейках JSON-формата
-from randan.tools import varPreprocessor
+
 columnsToJSON = varPreprocessor.jsonChecker(df)
 
 if len(columnsToJSON) > 0:
-    print('Обнаружены столбцы, содержащие внутри своих ячеек JSON-объекты; Excel не поддерживает JSON-формат;'
-          , 'чтобы формат JSON не потерялся, сохраняю эти столбцы в файл формата НЕ XLSX, а JSON. Остальные же столбцы сохраняю в файл формата XLSX')
+    print(
+        "Обнаружены столбцы, содержащие внутри своих ячеек JSON-объекты; Excel не поддерживает JSON-формат;",
+        "чтобы формат JSON не потерялся, сохраняю эти столбцы в файл формата НЕ XLSX, а JSON. Остальные же столбцы сохраняю в файл формата XLSX",
+    )
 
-    columnsToJSON.append('id')
-    if 'from_id' in df.columns: columnsToJSON.append('from_id')
-    if 'owner_id' in df.columns: columnsToJSON.append('owner_id')
+    columnsToJSON.append("id")
+    if "from_id" in df.columns:
+        columnsToJSON.append("from_id")
+    if "owner_id" in df.columns:
+        columnsToJSON.append("owner_id")
 
-    df[columnsToJSON].to_json(r"YT_раздельный сбор\20250311_0230_YT_раздельный сбор_videos_JSON_varS.json")
+    df[columnsToJSON].to_json(
+        r"YT_раздельный сбор\20250311_0230_YT_раздельный сбор_videos_JSON_varS.json"
+    )
 
-    columnsToJSON.remove('id')
-    if 'from_id' in columnsToJSON: columnsToJSON.remove('from_id')
-    if 'owner_id' in columnsToJSON: columnsToJSON.remove('owner_id')
+    columnsToJSON.remove("id")
+    if "from_id" in columnsToJSON:
+        columnsToJSON.remove("from_id")
+    if "owner_id" in columnsToJSON:
+        columnsToJSON.remove("owner_id")
 
-    df.drop(columnsToJSON, axis=1).to_excel(r"YT_раздельный сбор\20250311_0230_YT_раздельный сбор_videos_Other_varS.xlsx")
+    df.drop(columnsToJSON, axis=1).to_excel(
+        r"YT_раздельный сбор\20250311_0230_YT_раздельный сбор_videos_Other_varS.xlsx"
+    )
--- randan/tools/varPreprocessor.py
+++ randan/tools/varPreprocessor.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-'''
+"""
 A module for preprocessing variables of nominal, ordinal, interval, and higher-level measurement
 Авторский модуль для предобработки переменных номинального, порядкового, интервального и более высокого типа шкалы
-'''
+"""
 
 # sys & subprocess -- эти пакеты должны быть предустановлены. Если с ними какая-то проблема, то из этого скрипта решить их сложно
 import sys
@@ -14,84 +14,119 @@ from subprocess import check_call
 attempt = 0
 while True:
     try:
-        import matplotlib.pyplot as plt, pandas
+        import matplotlib.pyplot as plt
+        import pandas
+
         break
     except ModuleNotFoundError:
         errorDescription = sys.exc_info()
-        module = str(errorDescription[1]).replace("No module named '", '').replace("'", '') #.replace('_', '')
-        if '.' in module: module = module.split('.')[1]
+        module = (
+            str(errorDescription[1]).replace("No module named '", "").replace("'", "")
+        )  # .replace('_', '')
+        if "." in module:
+            module = module.split(".")[1]
         print(
-f'''Пакет {module} НЕ прединсталлирован, но он требуется для работы скрипта, поэтому будет инсталлирован сейчас
+            f"""Пакет {module} НЕ прединсталлирован, но он требуется для работы скрипта, поэтому будет инсталлирован сейчас
 Попытка № {attempt} из 10
-'''
-              )
+"""
+        )
         check_call([sys.executable, "-m", "pip", "install", module])
         attempt += 1
-        if  attempt == 10:
+        if attempt == 10:
             print(
-f'''Пакет {module} НЕ прединсталлирован; он требуется для работы скрипта, но инсталлировать его не удаётся,
+                f"""Пакет {module} НЕ прединсталлирован; он требуется для работы скрипта, но инсталлировать его не удаётся,
 поэтому попробуйте инсталлировать его вручную, после чего снова запустите скрипт
-'''
-                  )
+"""
+            )
             break
 
+
 def jsonChecker(dfIn):
-    '''
+    """
     Проверка всех столбцов датафрейма на наличие в них словарей или списков
-    '''
-    
+    """
+
     columnsToJSON = list(dfIn.columns)
-    for column in dfIn.columns: # цикл для прохода по всем столбцам датафрейма
+    for column in dfIn.columns:  # цикл для прохода по всем столбцам датафрейма
         # Если в столбце не встречаются ячейки со словарями или списками, то..
-        if dfIn[column].apply(lambda cellContent: True if (type(cellContent) == dict) | (type(cellContent) == list) else False).sum() == 0:
-            columnsToJSON.remove(column) # .. то этот столбец исключается из "подозреваемых" методм .remove() класса списков
+        if (
+            dfIn[column]
+            .apply(
+                lambda cellContent: True
+                if (type(cellContent) == dict) | (type(cellContent) == list)
+                else False
+            )
+            .sum()
+            == 0
+        ):
+            columnsToJSON.remove(
+                column
+            )  # .. то этот столбец исключается из "подозреваемых" методм .remove() класса списков
     return columnsToJSON
+
 
 def varHist(dfIn, var):
     plt.figure()
     plt.hist(dfIn[var])
-    plt.title(f'Values distribution of {var}')
-    plt.xlabel(f'{var}')
+    plt.title(f"Values distribution of {var}")
+    plt.xlabel(f"{var}")
     plt.xticks(dfIn[var].unique(), minor=True)
-    plt.ylabel('Frequency')
-    plt.show();
+    plt.ylabel("Frequency")
+    plt.show()
+
 
 def valuesDropper(dfIn, var):
     df = dfIn.copy()
-    print('В этой функции переменная рассматривается как порядковая или более высокого типа шкалы')
+    print(
+        "В этой функции переменная рассматривается как порядковая или более высокого типа шкалы"
+    )
     varHist(df, var)
-    print('\n--- Если требуется исключить из дальнейшего рассмотрения какие-то значения переменной, введите в окно МИНИМАЛЬНОЕ из этих значений и нажмите Enter'
-        , '\n--- В противном случае, просто нажмите Enter')
+    print(
+        "\n--- Если требуется исключить из дальнейшего рассмотрения какие-то значения переменной, введите в окно МИНИМАЛЬНОЕ из этих значений и нажмите Enter",
+        "\n--- В противном случае, просто нажмите Enter",
+    )
     valueForRemoving = input()
     if len(valueForRemoving) != 0:
         valueS_ForRemoving = [valueForRemoving]
-        print('\n--- Теперь введите МАКСИМАЛЬНОЕ из этих значений и нажмите Enter'
-            , '\n--- Если максимум равен минимуму, просто нажмите Enter')
+        print(
+            "\n--- Теперь введите МАКСИМАЛЬНОЕ из этих значений и нажмите Enter",
+            "\n--- Если максимум равен минимуму, просто нажмите Enter",
+        )
         valueForRemoving = input()
         if len(valueForRemoving) != 0:
             valueS_ForRemoving.append(valueForRemoving)
-            print('\n--- Если ВСЕ числа в обзначенном Вами диапазоне требуется исключить из дальнейшего рассмотрения, просто нажмите Enter'
-                , '\n--- В противном случае введите конкретное число из этого диапазона и нажмите Enter')
+            print(
+                "\n--- Если ВСЕ числа в обзначенном Вами диапазоне требуется исключить из дальнейшего рассмотрения, просто нажмите Enter",
+                "\n--- В противном случае введите конкретное число из этого диапазона и нажмите Enter",
+            )
             valueForRemoving = input()
             if len(valueForRemoving) != 0:
                 while len(valueForRemoving) != 0:
                     valueS_ForRemoving.append(valueForRemoving)
-                    print('\n--- Если требуется исключить из дальнейшего рассмотрения ЕЩЁ одно конкретное число из этого диапазона, введите его и нажмите Enter'
-                        , '\n--- В противном случае просто нажмите Enter')
+                    print(
+                        "\n--- Если требуется исключить из дальнейшего рассмотрения ЕЩЁ одно конкретное число из этого диапазона, введите его и нажмите Enter",
+                        "\n--- В противном случае просто нажмите Enter",
+                    )
                     valueForRemoving = input()
                     valueS_ForRemoving.append(valueForRemoving)
                 # Исключить из рассмотрения диапазон
-                valueS_ForRemoving.remove('')
+                valueS_ForRemoving.remove("")
                 for valueForRemoving in valueS_ForRemoving:
                     df = df[dataForML[var] != float(valueForRemoving)]
             else:
                 # Исключить из рассмотрения диапазон
                 valueS_ForRemoving.sort()
-                df = df[(df[var] < float(valueS_ForRemoving[0])) | (df[var] > float(valueS_ForRemoving[-1]))]
+                df = df[
+                    (df[var] < float(valueS_ForRemoving[0]))
+                    | (df[var] > float(valueS_ForRemoving[-1]))
+                ]
 
-        print('\nДля дальнейшего рассмотрения остались следующие значения игрека и их частоты:')
+        print(
+            "\nДля дальнейшего рассмотрения остались следующие значения игрека и их частоты:"
+        )
         varHist(df, var)
     return df
+
 
 # # 1.1.3 Если требуется выбрать только интересующие значения игрека
 # print('В этом чанке игрек рассматривается как номинальная переменная'
