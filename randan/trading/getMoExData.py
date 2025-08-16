beb7da59998ce059aef6a1c5c27a5f0b2f00f244
RandanCSS <80287949+RandanCSS@users.noreply.github.com>
2025-08-17 00:37:16 +0300

    Update getMoExData.py

--- randan/trading/getMoExData.py
+++ randan/trading/getMoExData.py
@@ -139,11 +139,13 @@ def getMoExData(
             if market == 'bonds':
                 marketdata_yieldS_additional = pseudojson2df(headerS, -1, url + f'/boards/{board}/securities')
                 marketdata_yieldS = pandas.concat([marketdata_yieldS, marketdata_yieldS_additional], ignore_index=True)
+                # print('marketdata_yieldS.columns:', marketdata_yieldS.columns) # для отладки   
 
             if market == 'forts':
                 marketdata_additional = pseudojson2df(headerS, 1, url + f'/boards/{board}/securities')
                 # display('marketdata_additional:', marketdata_additional) # для отладки
                 marketdata = pandas.concat([marketdata, marketdata_additional], ignore_index=True)
+                # print('marketdata.columns:', marketdata.columns) # для отладки   
 
         if os.path.exists(path + market + 'ColumnsDescriptionsSelected.xlsx'):
             columnsDescriptionS = pandas.read_excel(path + market + 'ColumnsDescriptionsSelected.xlsx')
@@ -153,12 +155,9 @@ def getMoExData(
             columnsDescriptionS = columnsDescriptionS[columnsDescriptionS['name'] !='BOARDID']
 
         columnsDescriptionS = columnsDescriptionS[columnsDescriptionS['name'].notna()]
-        columnsDescriptionS = columnsDescriptionS['name'].tolist()
+        columnsDescriptionS = columnsDescriptionS['name'].drop_duplicates().tolist()
         if market == 'bonds': columnsDescriptionS.append('URL')
     
-        # print('marketdata_yieldS.columns:', marketdata_yieldS.columns) # для отладки   
-        # print('marketdata.columns:', marketdata_yieldS.columns) # для отладки   
-
         if market == 'bonds':# securitieS = securitieS.merge(marketdata_yieldS, on='SECID')
             securitieS = securitieS.merge(marketdata_yieldS, on='SECID', suffixes=("", "_drop"), how="left")
             securitieS = securitieS[[column for column in securitieS.columns if not column.endswith("_drop")]]
@@ -170,6 +169,7 @@ def getMoExData(
             # print('securitieS.columns:', securitieS.columns) # для отладки
 
         securitieS = securitieS.groupby('SECID', as_index=False).first()
+        # print('securitieS.columns:', securitieS.columns) # для отладки
         if market == 'bonds': securitieS['URL'] = 'https://www.moex.com/ru/issue.aspx?code=' + securitieS['ISIN']
         # print('securitieS.columns:', securitieS.columns) # для отладки
         securitieS = securitieS[columnsDescriptionS]
