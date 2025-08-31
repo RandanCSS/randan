1e96ce2610143a73358ef0b442004b33714f0506
RandanCSS <80287949+RandanCSS@users.noreply.github.com>
2025-08-31 15:14:37 +0300

    Update forSelenium.py

--- randan/tools/forSelenium.py
+++ randan/tools/forSelenium.py
@@ -1,3 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+'''
+A module for facilitating the use of selenium
+'''
+# import sys
+# sys.path.append(r"C:\Users\Alexey\Dropbox\Мои\RAnDan\myModules")
+
+# sys & subprocess -- эти пакеты должны быть предустановлены. Если с ними какая-то проблема, то из этого скрипта решить их сложно
+import sys
+from subprocess import check_call
+
+# --- остальные модули и пакеты
+attempt = 0
+while True:
+    try:
+        from pandas import DataFrame
+        break
+    except ModuleNotFoundError:
+        errorDescription = sys.exc_info()
+        module = str(errorDescription[1]).replace("No module named '", '').replace("'", '') #.replace('_', '')
+        print(
+f'''Пакет {module} НЕ прединсталлирован, но он требуется для работы скрипта, поэтому будет инсталлирован сейчас
+Попытка № {attempt} из 10
+'''
+              )
+        check_call([sys.executable, "-m", "pip", "install", module])
+        attempt += 1
+        if  attempt == 10:
+            print(
+f'''Пакет {module} НЕ прединсталлирован; он требуется для работы скрипта, но инсталлировать его не удаётся,
+поэтому попробуйте инсталлировать его вручную, после чего снова запустите скрипт
+'''
+                  )
+            break
+            
 def blockSearch(attemptsMax, text, xPathS):
     block = None 
     trCounter = 1
