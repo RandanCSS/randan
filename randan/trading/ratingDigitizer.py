b5a37697310d385779e45c4e4223870e20b4c342
RandanCSS <80287949+RandanCSS@users.noreply.github.com>
2025-12-20 18:35:21 +0300

    Update ratingDigitizer.py

--- randan/trading/ratingDigitizer.py
+++ randan/trading/ratingDigitizer.py
@@ -1,4 +1,37 @@
-import re
+# Авторский модуль для перевода в число рейтинга с эмитентов торгуемых на МосБирже облигаций
+
+# 0. Активировать требуемые для работы скрипта модули и пакеты 
+# sys & subprocess -- эти пакеты должны быть предустанавлены. Если с ними какая-то проблема, то из этого скрипта решить их сложно
+import sys
+from subprocess import check_call
+
+# --- остальные модули и пакеты
+while True:
+    try:
+        from randan.tools import coLabAdaptor, textPreprocessor # авторские модули для (а) адаптации текущего скрипта к файловой системе CoLab и (б) предобработки нестандартизированнрого текста
+        import os, pandas, re
+        break
+    except ModuleNotFoundError:
+        errorDescription = sys.exc_info()
+        module = str(errorDescription[1]).replace("No module named '", '').replace("'", '') #.replace('_', '')
+        if '.' in module: module = module.split('.')[0]
+        print(
+f'''Пакет {module} НЕ прединсталлирован, но он требуется для работы скрипта, поэтому будет инсталлирован сейчас
+Попытка № {attempt} из 10
+'''
+              )
+        check_call([sys.executable, "-m", "pip", "install", module])
+        attempt += 1
+        if  attempt == 10:
+            print(
+f'''Пакет {module} НЕ прединсталлирован; он требуется для работы скрипта, но инсталлировать его не удаётся,
+поэтому попробуйте инсталлировать его вручную, после чего снова запустите скрипт
+'''
+                  )
+            break
+
+coLabFolder = coLabAdaptor.coLabAdaptor()
+
 def ratingDigitizer(letters, raitingSource):
     if raitingSource == 'RB':
         letters = letters.replace(' ГМ', '').replace(' Неквалы', '').replace(' Остальные', '')
