0d5356e61b839a64fdd4715a2daed26e96bf6033
RandanCSS <80287949+RandanCSS@users.noreply.github.com>
2025-12-29 00:52:16 +0300

    Update issuerProcessor.py

--- randan/trading/issuerProcessor.py
+++ randan/trading/issuerProcessor.py
@@ -82,17 +82,18 @@ def issuerExtractor(dfIn):
     df = dfIn.copy()
     df['Эмитент'] = df['SECNAME'].str.replace('_', ' ').str.replace('-', ' ')
     df['Эмитент'] = df['Эмитент'].str.replace('ЯНАО', 'ЯНАвОк')
-    df['Эмитент'] = df['Эмитент'].apply(lambda text: re.sub(r' [БЗ][OОPР]П?.+', '', text))
-    df['Эмитент'] = df['Эмитент'].apply(lambda text: re.sub(r'Б\d+', '', text))
-    df['Эмитент'] = df['Эмитент'].apply(lambda text: re.sub(r' 0\d+.*', '', text))
-    df['Эмитент'] = df['Эмитент'].apply(lambda text: re.sub(r' \d+ обл\.?', '', text))
+    df.loc[df['Эмитент'].str.contains('СберИОС'), 'Эмитент'] = 'СберИОС'
+    df['Эмитент'] = df['Эмитент'].apply(lambda cellContent: re.sub(r' [БЗ][OОPР]П?.+', '', cellContent))
+    df['Эмитент'] = df['Эмитент'].apply(lambda cellContent: re.sub(r'Б\d+', '', cellContent))
+    df['Эмитент'] = df['Эмитент'].apply(lambda cellContent: re.sub(r' 0\d+.*', '', cellContent))
+    df['Эмитент'] = df['Эмитент'].apply(lambda cellContent: re.sub(r' \d+ обл\.?', '', cellContent))
     df['Эмитент'] = df['Эмитент'].apply(textPreprocessor.simbolsCleaner)
     df['Эмитент'] = df['Эмитент'].str.replace('ПАО ', '').str.replace(' ПАО', '')\
         .str.replace('АО ', '').str.replace(' АО', '')\
         .str.replace('ООО ', '').str.replace(' ООО', '')\
         .str.replace('"', '')
-    df['Эмитент'] = df['Эмитент'].apply(lambda text: text if text[0] != ' ' else text[1:])
-    df['Эмитент'] = df['Эмитент'].apply(lambda text: text if text[-1] != ' ' else text[:-1])
+    df['Эмитент'] = df['Эмитент'].apply(lambda cellContent: cellContent if cellContent[0] != ' ' else cellContent[1:])
+    df['Эмитент'] = df['Эмитент'].apply(lambda cellContent: cellContent if cellContent[-1] != ' ' else cellContent[:-1])
     df['Эмитент'] = df['Эмитент'].str.split(' Б ').str[0]
     df['Эмитент'] = df['Эмитент'].str.lower()
     df.loc[df['Эмитент'].str.contains('офз'), 'Эмитент'] = 'минфин рф'
