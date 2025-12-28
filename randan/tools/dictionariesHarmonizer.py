d211249601b35ae4cb2ca62a5a93b8dfbcb4d44e
RandanCSS <80287949+RandanCSS@users.noreply.github.com>
2025-12-29 00:09:20 +0300

    Update dictionariesHarmonizer.py

--- randan/tools/dictionariesHarmonizer.py
+++ randan/tools/dictionariesHarmonizer.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 '''
-A module for editing one dataframe (df_small) within its specific column based on the same column from another dataframe (df_large)
+A module for editing one dataframe (df_editing) within its specific column based on the same column from another dataframe (df_standard)
 '''
 # import sys
 # sys.path.append(r"C:\Users\Alexey\Dropbox\Мои\RAnDan\myModules")
@@ -35,23 +35,23 @@ f'''Пакет {module} НЕ прединсталлирован; он требу
                   )
             break
 
-def dictionariesHarmonizer(df_large, dfIn_Small, columnName):
-    df_small = dfIn_Small.copy() # df_small -- датафрейм, редактируемый в столбце columnName на основе того же столбца датафрейма df_large
+def dictionariesHarmonizer(df_standard, dfIn_Small, columnName):
+    df_editing = dfIn_Small.copy() # df_editing -- датафрейм, редактируемый в столбце columnName на основе того же столбца датафрейма df_standard
 
     # Шаг № 1. Грубая сверка
-    df_small_matching = df_small[df_small[columnName].isin(df_large[columnName])]
-    df_small_New_1 = df_small[df_small[columnName].isin(df_large[columnName]) != True]
+    df_editing_matching = df_editing[df_editing[columnName].isin(df_standard[columnName])]
+    df_editing_New_1 = df_editing[df_editing[columnName].isin(df_standard[columnName]) != True]
 
     # Шаг № 2. Тонкая сверка
     rowS_toDrop = []
-    df_small_New_2 = df_small_New_1.copy()
-    elementS_Small = df_small_New_1[columnName]
+    df_editing_New_2 = df_editing_New_1.copy()
+    elementS_Small = df_editing_New_1[columnName]
     for element_Small in elementS_Small:
-        for element_Large in df_large[columnName]:
+        for element_Large in df_standard[columnName]:
             if element_Large in element_Small:
-                df_small_New_1.loc[df_small_New_1[columnName] == element_Small, columnName] = element_Large # заменить element_Small на element_Large ,
-                    # что обеспечивает совместимость обрабатываемых тут строчек df_small_New_1 b df_large
-                df_small_New_2 = df_small_New_2[df_small_New_2[columnName] != element_Small] 
-    return df_small_matching, df_small_New_1, df_small_New_2
-    # df_small_New_1 -- часть редактируемого датафрейма (df_small), которая не прошла грубую сверку, но прошла тонкую сверку
-    # df_small_New_2 -- часть редактируемого датафрейма (df_small), которая не прошла ни грубую, ни тонкую сверку
+                df_editing_New_1.loc[df_editing_New_1[columnName] == element_Small, columnName] = element_Large # заменить element_Small на element_Large ,
+                    # что обеспечивает совместимость обрабатываемых тут строчек df_editing_New_1 b df_standard
+                df_editing_New_2 = df_editing_New_2[df_editing_New_2[columnName] != element_Small] 
+    return df_editing_matching, df_editing_New_1, df_editing_New_2
+    # df_editing_New_1 -- часть редактируемого датафрейма (df_editing), которая не прошла грубую сверку, но прошла тонкую сверку
+    # df_editing_New_2 -- часть редактируемого датафрейма (df_editing), которая не прошла ни грубую, ни тонкую сверку
