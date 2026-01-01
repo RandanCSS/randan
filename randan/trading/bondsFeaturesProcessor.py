0d2031bad3dfe25fb78ac18565dd4d8b427c9cf2
RandanCSS <80287949+RandanCSS@users.noreply.github.com>
2026-01-01 20:04:48 +0300

    Update bondsFeaturesProcessor.py

--- randan/trading/bondsFeaturesProcessor.py
+++ randan/trading/bondsFeaturesProcessor.py
@@ -73,9 +73,9 @@ def bondsFeaturesProcessor(
     # display(bondS) # для отладки
 
 # 1.2 Рейтинг и другие важные характеристики из bondsRatingS
-    fileUptodateName_0 = files2df.getFileUptodateName('_bondsRatingS', None, path + 'Замеры рейтингов')
+    fileUptodateName_0 = files2df.getFileUptodateName('_bondsRatingS.', None, path + 'Замеры рейтингов')
     # print('fileUptodateName_0:', fileUptodateName_0) # для отладки
-    fileUptodateName_1 = files2df.getFileUptodateName('_bondsRatingS', [fileUptodateName_0], path + 'Замеры рейтингов')
+    fileUptodateName_1 = files2df.getFileUptodateName('_bondsRatingS.', [fileUptodateName_0], path + 'Замеры рейтингов')
 
     # if os.path.exists(path + 'Замеры рейтингов'):
     #     # print("Директория 'Замеры рейтингов' существует") # для отладки
