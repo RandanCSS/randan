f63b2a25539a0a04a98348f3a3418e0b6e6928d4
RandanCSS <80287949+RandanCSS@users.noreply.github.com>
2026-07-27 13:54:55 +0300

    Update issuerProcessor.py

--- randan/trading/issuerProcessor.py
+++ randan/trading/issuerProcessor.py
@@ -12,7 +12,7 @@ for attempt in range(1, 4):
         # (а) адаптации текущего скрипта к файловой системе CoLab и
         # (б) предобработки нестандартизированнрого текста
         import os, pandas, re
-        break
+        break # выход из цикла for attempt in range(3)
     except ModuleNotFoundError:
         errorDescription = sys.exc_info()
         module = str(errorDescription[1]).replace("No module named '", '').replace("'", '') #.replace('_', '')
