3eb67a073ce6ae698e2f1a6135cf1530d10d8681
RandanCSS <80287949+RandanCSS@users.noreply.github.com>
2026-08-17 20:56:58 +0300

    Update bondsFeaturesProcessor.py

--- randan/trading/bondsFeaturesProcessor.py
+++ randan/trading/bondsFeaturesProcessor.py
@@ -800,4 +800,7 @@ def bondsFeaturesProcessor(attemptsMax,
     print('Компоненты специфики: валюта, сектор рынка, амортизация, определён ли купон')
     display(bondS['Специфика'].value_counts().sort_index())
 
+    driverCloser(driver)
+    driverCloser(driver_CB)
+    driverCloser(driver_TB)
     if returnDfs: return bondS, issuerS_withActualRating, issuerS_withActualRating_change
