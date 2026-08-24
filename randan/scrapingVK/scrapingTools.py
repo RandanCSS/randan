613ce891a8bdeb4ed9737da3035cdcb2d732e5ef
RandanCSS <80287949+RandanCSS@users.noreply.github.com>
2026-08-25 01:51:17 +0300

    Update scrapingTools.py

--- randan/scrapingVK/scrapingTools.py
+++ randan/scrapingVK/scrapingTools.py
@@ -22,3 +22,76 @@ def dfColumnsProcessor(df_in, response):
                     df = fieldsProcessor(dfIn=df, fieldsColumn=fieldsColumn, response=response)
 
     return df
+
+def errorProcessor(API_keyS, keyOrder, pause, response, tryer):
+    goC = True
+    goS = True
+
+    if 'error' in response.keys():
+        if 'Application is blocked' in response['error']['error_msg']:
+            # print('  keyOrder до замены', '                    ') # для отладки
+
+            keyOrder = keyOrder + 1 if keyOrder < (len(API_keyS) - 1) else 0 # смена ключа, если есть на что менять
+            print(
+f'''
+Похоже, ключ попал под ограничение вследствие блокировки приложения, к которому он относится; пробую перейти к следующему ключу (№ {keyOrder})'''
+                  )
+            # print('  keyOrder после замены', keyOrder, '                    ') # для отладки
+
+            tryer += 1
+            if tryer >= len(API_keyS):
+                print(
+'''
+Попробовал все располагаемые ключи; все они заблокированны или неактивны(
+Попробуйте обновить сервисный ключ в Вашем приложении API ВК,
+после чего замените старые ключи новым в файле credentialsVK.txt и перезапустите этот скрипт'''
+                      )
+                # response = {'items': [], 'total_count': 0} # принудительная выдача для response
+                goS = False # нет смысла продолжать исполнение скрипта
+                goC = False # и, следовательно, нет смысла в новых итерациях цикла while goC
+
+        elif 'Too many requests per second' in response['error']['error_msg']:
+            # print('  keyOrder до замены', '                    ') # для отладки
+
+            keyOrder = keyOrder + 1 if keyOrder < (len(API_keyS) - 1) else 0 # смена ключа, если есть на что менять
+            print(
+f'''
+Похоже, ключ попал под ограничение вследствие слишком высокой частоты обращения скрипта к API;
+пробую перейти к следующему ключу (№ {keyOrder}) и снизить частоту'''
+                  )
+            # print('  keyOrder после замены', keyOrder, '                    ') # для отладки
+
+            pause += 0.25
+
+        elif 'Unknown application: could not get application' in response['error']['error_msg']:
+            # print('  keyOrder до замены', '                    ') # для отладки
+
+            keyOrder = keyOrder + 1 if keyOrder < (len(API_keyS) - 1) else 0 # смена ключа, если есть на что менять
+            print('\nПохоже, Ваше ВК-приложение попало под ограничение; пробую перейти к следующему ключу (№ {keyOrder}) и снизить частоту')
+            # print('  keyOrder после замены', keyOrder, '                    ') # для отладки
+            pause += 0.25
+
+        elif 'Internal server error: Unknown error, try later' in response['error']['error_msg']:
+            print('\nПохоже, ошибка на сервере ВК; подождите и запустите скрипт с начала')
+            response = {'items': [], 'total_count': 0} # принудительная выдача для response
+            goS = False # нет смысла продолжать исполнение скрипта
+            goC = False # и, следовательно, нет смысла в новых итерациях цикла while goC
+
+        elif 'User authorization failed' in response['error']['error_msg']:
+            print(
+'''
+Похоже, аккаунт попал под ограничение. Оно может быть снято с аккаунта сразу или спустя какое-то время.
+Подождите или подготовьте новый ключ в другом аккаунте. И запустите скрипт с начала'''
+                  )
+            response = {'items': [], 'total_count': 0} # принудительная выдача для response
+            goS = False # нет смысла продолжать исполнение скрипта
+            goC = False # и, следовательно, нет смысла в новых итерациях цикла while goC
+
+        else:
+            print('  Похоже, проблема НЕ в слишком высокой частоте обращения скрипта к API((')
+            print('  ', response['error']['error_msg'])
+            response = {'items': [], 'total_count': 0} # принудительная выдача для response
+            goS = False # нет смысла продолжать исполнение скрипта
+            goC = False # и, следовательно, нет смысла в новых итерациях цикла while goC
+
+    return goC, goS, keyOrder, pause, response, tryer
