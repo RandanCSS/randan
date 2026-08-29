291001cd2f299fd2da6f720c16467cfc17f3242c
RandanCSS <80287949+RandanCSS@users.noreply.github.com>
2026-08-29 23:52:18 +0300

    Update wallGetComments.py

--- randan/scrapingVK/wallGetComments.py
+++ randan/scrapingVK/wallGetComments.py
@@ -85,7 +85,7 @@ f'''Поскольку исполнение скрипта натолкнуло�
 
 # Сохранение следа исполнения скрипта, натолкнувшегося на ошибку, непосредственно в директорию Temporal в текущей директории
         if not fields: fields = []
-        with open(f'{momentCurrent.strftime('%Y%m%d')}{complicatedNamePart}_Temporal{slash}fields.txt', 'w', encoding='utf-8') as file:
+        with open(f"{momentCurrent.strftime('%Y%m%d')}{complicatedNamePart}_Temporal{slash}fields.json", 'w', encoding='utf-8') as file:
             json.dump(data_to_save, file, ensure_ascii=False, indent=4)
 
         scrapingTools.containerExport(f"{momentCurrent.strftime('%Y%m%d')}{complicatedNamePart}_Temporal{slash}filter.txt", filter if filter else '')
