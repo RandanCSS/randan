ce712fbd68c275cc8c564b233e1785f54fcaf329
RandanCSS <80287949+RandanCSS@users.noreply.github.com>
2026-08-30 00:02:56 +0300

    Update newsFeedSearch.py

--- randan/scrapingVK/newsFeedSearch.py
+++ randan/scrapingVK/newsFeedSearch.py
@@ -145,8 +145,8 @@ f'''Поскольку исполнение скрипта натолкнуло�
 
 # Сохранение следа исполнения скрипта, натолкнувшегося на ошибку, непосредственно в директорию Temporal в текущей директории
         if not fields: fields = []
-        with open(f"{momentCurrent.strftime('%Y%m%d')}{complicatedNamePart}_Temporal{slash}fields.txt", 'w', encoding='utf-8') as file:
-            json.dump(data_to_save, file, ensure_ascii=False, indent=4)
+        with open(f"{momentCurrent.strftime('%Y%m%d')}{complicatedNamePart}_Temporal{slash}fields.json", 'w', encoding='utf-8') as file:
+            json.dump(fields, file, ensure_ascii=False, indent=4)
 
         scrapingTools.containerExport(f"{momentCurrent.strftime('%Y%m%d')}{complicatedNamePart}_Temporal{slash}method.txt", method)
         scrapingTools.containerExport(f"{momentCurrent.strftime('%Y%m%d')}{complicatedNamePart}_Temporal{slash}q.txt", q if q else '')
